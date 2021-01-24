a9d806975423fa74caed0426025906d8b88d613b
3100ashley <60107314+3100ashley@users.noreply.github.com>
2021-01-24 13:22:43 -0800

    Add files via upload
    initial file and started js

---
+++ index.js
@@ -0,0 +1,16 @@
+let imageArr = ["flower","fruit","rainbow","sun"];
+$(".box").click(function(){
+  alert("hello");
+});
+
+$("#btn").click(function(){
+  $("#btn").text("0");
+});
+
+function startOfGame(){
+  let random = Math.floor(Math.random() * 8) + 1;
+  
+
+  ("image/"+imageArr[])
+
+}
