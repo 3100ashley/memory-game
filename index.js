7a65764cab519554fb37093858379442f39b9cbf
3100ashley <60107314+3100ashley@users.noreply.github.com>
2021-02-09 11:59:21 -0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,41 +1,84 @@
-let imageArr = ["flower","fruit","rainbow","sun"];
+//Issue: Click many boxes.
 
-let firstClick = [];
+let boxClass = [];
+let imgSources = [];
 
 let startGame = false;
+let score = 0;
+var count = 0;
 $("img").css("visibility","hidden");
 
 $("#btn").click(function(){
 
     if(!startGame){
-      $("#btn").text("0");
+      count = 0;
+      score = 0;
+      boxClass = [];
+      imgSources = [];
+      $("#btn").text(score);
       shuffle();
       $("img").css("visibility","visible");
       startGame = true;
       setTimeout(function(){
         $("img").css("visibility","hidden");
+        boxClick();
+
       },3000);
+
     }
 });
 
-var count = 0;
-$(".box").click(function(){
-  var imgSrc = $("img", this).attr("src")
-  count++;
-  if(count <= 2){
-    $("img", this).css("visibility", "visible");
-    firstClick.push(imgSrc);
+function boxClick(){
+  $(".box").click(function(){
+    var imgSrc = $("img", this).attr("src")
+    count++;
+    console.log("click count: " + count);
+    if(count <= 2){
+      $("img", this).css("visibility", "visible");
+      imgSources.push(imgSrc);
+      boxClass.push(this);
+    }
+    if(count === 2){
+    console.log("clickCount check: " + count);
+    solutionCheck(imgSources, this);
   }
-  solutionCheck(firstClick);
-});
+  });
+}
 
-function solutionCheck(array){
+
+
+
+function solutionCheck(array, boxObject){
   let length = array.length;
   if(array[length - 1] === array[length - 2]){
     console.log("correct");
-  }
-}
+    count = 0;
+    score++;
+    // console.log("score: " +score);
+    // console.log("count: " +count);
+    $("#btn").text(score);
+    if(score == 4){
 
+      startGame = false;
+      $("#btn").text("Winner!! click me to play again");
+      $("#btn").click(document.location.reload(true));
+
+    }
+  }
+  else {
+    console.log("incorrect");
+
+    setTimeout(function(){
+      let bCLength = boxClass.length;
+      count = 0;
+      $("img", boxClass[bCLength - 1]).css("visibility", "hidden")
+      $("img", boxClass[bCLength - 2]).css("visibility", "hidden")
+    }, 3000);
+
+
+  }
+
+}
 
 function shuffle(){
   for(let i = 1; i <= 8; i++){
