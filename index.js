8505ae3b85c63b9a2b5d3c28363c27b7d1a7a230
pablo-graham <72041526+pablo-graham@users.noreply.github.com>
2021-02-16 21:04:36 -0800

    Add files via upload
    fixed the first problem of being able to click the same box and win, need to fix ending

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ let boxClass = [];
 let imgSources = [];
 
 let startGame = false;
+let ableToClick = true;
 let score = 0;
 var count = 0;
 $("img").css("visibility","hidden");
@@ -24,59 +25,64 @@ $("#btn").click(function(){
         boxClick();
 
       },3000);
-
-    }
+  }
 });
 
-function boxClick(){
-  $(".box").click(function(){
-    var imgSrc = $("img", this).attr("src")
-    count++;
-    console.log("click count: " + count);
-    if(count <= 2){
-      $("img", this).css("visibility", "visible");
-      imgSources.push(imgSrc);
-      boxClass.push(this);
-    }
-    if(count === 2){
-    console.log("clickCount check: " + count);
-    solutionCheck(imgSources, this);
-  }
-  });
-}
 
-
-
-
-function solutionCheck(array, boxObject){
-  let length = array.length;
-  if(array[length - 1] === array[length - 2]){
-    console.log("correct");
-    count = 0;
-    score++;
-    $("#btn").text(score);
-    if(score == 4){
-
-      $("#btn").text("Winner!! click me to play again");
-      $("#btn").click(function(){
-        document.location.reload(true)
+  function boxClick(){
+    if(ableToClick){
+      $(".box").click(function(){
+        var imgSrc = $("img", this).attr("src")
+        count++;
+        console.log(this);
+        if(count <= 2){
+          $("img", this).css("visibility", "visible");
+          imgSources.push(imgSrc);
+          boxClass.push(this);
+        }
+        if(count === 2){
+        console.log("clickCount check: " + count);
+        solutionCheck(imgSources, this);
+        }
       });
-
     }
   }
-  else {
-    console.log("incorrect");
 
-    setTimeout(function(){
-      let bCLength = boxClass.length;
+
+
+  function solutionCheck(array, boxObject){
+    let length = array.length;
+    if(array[length - 1] === array[length - 2] && boxClass[length - 1] != boxClass[length - 2]){
+      console.log("correct");
       count = 0;
-      $("img", boxClass[bCLength - 1]).css("visibility", "hidden")
-      $("img", boxClass[bCLength - 2]).css("visibility", "hidden")
-    }, 3000);
+      score++;
+      console.log("score: " + score);
+      $("#btn").text(score);
+      if(score >= 4){
+        //start=true;
+        clickAble();
+        $("#btn").text("Winner!! click me to play again");
+        $("#btn").click(function(){
+          document.location.reload(true)
+        });
 
+      }
+    }
+    else {
+      console.log("incorrect");
 
+      setTimeout(function(){
+        let bCLength = boxClass.length;
+        count = 0;
+        $("img", boxClass[bCLength - 1]).css("visibility", "hidden")
+        $("img", boxClass[bCLength - 2]).css("visibility", "hidden")
+      }, 3000);
+    }
   }
 
+
+function clickAble(){
+  ableToClick = false;
 }
 
 function shuffle(){
